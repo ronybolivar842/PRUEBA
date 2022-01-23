e69be571107a3df0430857bf3e72e6e0bc0c35c6
ronybolivar842 <rony-bolivar-842@hotmail.com>
2022-01-23 15:46:30 -0500

    primer

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+alert("hola javascript");
+// prompt("escribe el lenguaje de programacion");
+//confirm("estas seguro de que quieres salir de esta pagina");
+
+//variables - metodos(funciones) - clases
+//palabras()
